1b6d3ed22df71375d40de4df39ff651442a857a7
luojingwen <luojingwen@tanyue.cc>
2025-01-21 10:08:32 +0800

    创建任务

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # airflow_sp
 Airflow运行的sp后台任务
 
+- .env
+```angular2html
+AIRFLOW_UID=0
+AIRFLOW__CORE__FERNET_KEY=''
+
+AIRFLOW__CELERY__RESULT_BACKEND=''
+AIRFLOW__CELERY__BROKER_URL=''
+AIRFLOW__DATABASE__SQL_ALCHEMY_CONN=''
+
+_AIRFLOW_WWW_USER_USERNAME=''
+_AIRFLOW_WWW_USER_PASSWORD=''
+```
+
 - _AIRFLOW_WWW_USER_USERNAME:
   init时设置初始用户，但是在用户表已存在初始用户时不会**重新创建或更新**
--- dags/scheduled_dags.py
+++ dags/scheduled_dags.py
@@ -1,1 +1,39 @@
 import sys
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+sys.path.append('project/')
+from apps.modules.staff.timer import update_feishu_info, synchronization_point
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2025, 1, 19),
+    'retries': 0,
+    'retry_delay': timedelta(minutes=1),
+}
+
+update_feishu_info_dags = PythonOperator(
+    task_id='update_feishu_info',
+    python_callable=update_feishu_info,
+    dag=DAG(
+        'update_feishu_info',
+        default_args=default_args,
+        description='同步飞书员工信息',
+        schedule_interval="7 23 * * *",
+        catchup=False,
+    ),
+)
+
+synchronization_point_dags = PythonOperator(
+    task_id='synchronization_point',
+    python_callable=synchronization_point,
+    dag=DAG(
+        'synchronization_point',
+        default_args=default_args,
+        description='积分兑换奖品',
+        schedule_interval="*/2 * * * *",
+        catchup=False,
+    ),
+)
