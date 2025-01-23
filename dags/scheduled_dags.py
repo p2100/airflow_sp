e6c960e628fc27c5c25c22621f0d58126a212a01
luojingwen <luojingwen@tanyue.cc>
2025-01-23 14:37:44 +0800

    调整dag

--- dags/scheduled_dags.py
+++ dags/scheduled_dags.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-sys.path.append('project/')
+sys.path.insert(0, 'project/')
 from apps.modules.staff.timer import update_feishu_info, synchronization_point
 
 default_args = {
@@ -14,26 +14,30 @@ default_args = {
     'retry_delay': timedelta(minutes=1),
 }
 
+update_feishu_info_dag = DAG(
+    'update_feishu_info',
+    default_args=default_args,
+    description='同步飞书员工信息',
+    schedule_interval="7 23 * * *",
+    catchup=False,
+)
+
 update_feishu_info_dags = PythonOperator(
     task_id='update_feishu_info',
     python_callable=update_feishu_info,
-    dag=DAG(
-        'update_feishu_info',
-        default_args=default_args,
-        description='同步飞书员工信息',
-        schedule_interval="7 23 * * *",
-        catchup=False,
-    ),
+    dag=update_feishu_info_dag
+)
+
+synchronization_point_dag = DAG(
+    'synchronization_point',
+    default_args=default_args,
+    description='积分兑换奖品',
+    schedule_interval="*/2 * * * *",
+    catchup=False,
 )
 
 synchronization_point_dags = PythonOperator(
     task_id='synchronization_point',
     python_callable=synchronization_point,
-    dag=DAG(
-        'synchronization_point',
-        default_args=default_args,
-        description='积分兑换奖品',
-        schedule_interval="*/2 * * * *",
-        catchup=False,
-    ),
+    dag=synchronization_point_dag
 )
